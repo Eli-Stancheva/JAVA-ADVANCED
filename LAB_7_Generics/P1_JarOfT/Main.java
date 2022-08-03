c7ef74a700c87c75b8be6d398a66c00fbb0bc195
Elitsa Stancheva <99178411+Eli-Stancheva@users.noreply.github.com>
2022-08-03 09:45:39 +0300

    Add files via upload

---
+++ LAB_7_Generics/P1_JarOfT/Main.java
@@ -0,0 +1,13 @@
+package ADVANCED.LAB_7_Generics.P1_JarOfT;
+
+public class Main {
+    public static void main(String[] args) {
+        Jar<Integer> jar = new Jar<>();
+        jar.add(12);
+        jar.add(13);
+        jar.add(14);
+
+        System.out.println(jar.remove());
+        System.out.println(jar.remove());
+    }
+}
