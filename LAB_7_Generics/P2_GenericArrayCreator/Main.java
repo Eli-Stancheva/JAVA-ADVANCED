cd6d29866c64bb05ed0dca7d6ba1d337c0864ebc
Elitsa Stancheva <99178411+Eli-Stancheva@users.noreply.github.com>
2022-08-03 09:46:14 +0300

    Add files via upload

---
+++ LAB_7_Generics/P2_GenericArrayCreator/Main.java
@@ -0,0 +1,8 @@
+package ADVANCED.LAB_7_Generics.P2_GenericArrayCreator;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] strings = ArrayCreator.create(10, "none");
+        Integer[] integers = ArrayCreator.create(Integer.class, 10, 0);
+    }
+}
